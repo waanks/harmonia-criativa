4b492fb5ffe4f2ff6a8ef7ff2c6ca86065dce1d4
Mateus Oliveira <mateus.oliveira.external@realizecfi.com.br>
2026-06-01 21:16:48 -0300

    primeira integracao com o backend

---
+++ src/app/core/services/aluno-service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from "@angular/core";
+import { AlunoResponse } from "../models/aluno.model";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AlunoService {
+  private http = inject(HttpClient);
+  private apiUrl = "http://harmonia-api-production.up.railway.app/alunos";
+
+  listarTodos(): Observable<AlunoResponse[]> {
+    return this.http.get<AlunoResponse[]>(this.apiUrl);
+  }
+}
